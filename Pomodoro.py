d2d6f45f1dc69e4f48bc36ef4c43d185b928c2ec
Tobgen69 <tobgen69@users.noreply.github.com>
2018-10-12 20:31:42 +0200

    Initialized .gitignore and cleaned some code and added a test file.

--- Pomodoro.py
+++ Pomodoro.py
@@ -74,84 +74,8 @@ class Application(tk.Frame): # initialize a class to use later to create the app
         """
         winsound.MessageBeep()    
 
-
-    #pass 
-
-# region old window defs
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-    #          text='Local Time').grid(row=0, column=0) # create label with simple text
-    # self.LocalDate = tk.StringVar() # initialize textvariable for later use
-    # self.LocalDate.set('waiting...') # set text variable "default" value
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-    #          textvariable=self.LocalDate).grid(row=0, column=1) # create label to display the LocalDate
-
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-    #          text='Local Date').grid(row=1, column=0) # label for Local Date
-    # self.LocalTime = tk.StringVar() # init variable for localtime
-    # self.LocalTime.set('waiting...') # set default localtime
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-    #          textvariable=self.LocalTime).grid(row=1, column=1) 
-
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-    #          text='GMT Time').grid(row=2, column=0)
-    # self.nowGdate = tk.StringVar()
-    # self.nowGdate.set('waiting...')
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-    #          textvariable=self.nowGdate).grid(row=2, column=1)
-
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-    #          text='GMT Date').grid(row=3, column=0)
-    # self.nowGtime = tk.StringVar()
-    # self.nowGtime.set('waiting...')
-    # tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-    #          textvariable=self.nowGtime).grid(row=3, column=1)
-#endregion old window defs
-    
-    
-    #self.gettime() # always run gettime after finishing the button/label definitions
-# region old gettime def
-#     def gettime(self):
-#     gdt, gtm, ldt, ltm = GetDateTime() # get current date time variables etc.
-#     gdt = gdt[0:4] + '/' + gdt[4:6] + '/' + gdt[6:8]
-#     gtm = gtm[0:2] + ':' + gtm[2:4] + ':' + gtm[4:6] + ' Z'  
-#     ldt = ldt[0:4] + '/' + ldt[4:6] + '/' + ldt[6:8]
-#     ltm = ltm[0:2] + ':' + ltm[2:4] + ':' + ltm[4:6]  
-#     self.nowGtime.set(gdt) # set variables which are used in the labels of tkinter (textvariable)
-#     self.nowGdate.set(gtm) # set variables which are used in the labels of tkinter (textvariable)
-#     self.LocalTime.set(ldt) # set variables which are used in the labels of tkinter (textvariable)
-#     self.LocalDate.set(ltm) # set variables which are used in the labels of tkinter (textvariable)
-
-#     self.after(10000, self.gettime) # rerun the gettime method after .1 seconds 
-#    #print (ltm)  # Prove it is running this and the external code, too.
-#   pass
-#endregion old gettime def
-    
-
 root = tk.Tk() # initialize main tk-window of an application
 root.wm_title('Pomodoro Timer') # set title
 app = Application(master=root) # initialize app object
-
-# region other stuff old
-# w = 200 # width for the Tk root
-# h = 125 # height for the Tk root
-
-# # get display screen width and height
-# ws = root.winfo_screenwidth()  # width of the screen
-# hs = root.winfo_screenheight() # height of the screen
-
-# # calculate x and y coordinates for positioning the Tk root window
-
-# #centered
-# #x = (ws/2) - (w/2)
-# #y = (hs/2) - (h/2)
-
-# #right bottom corner (misfires in Win10 putting it too low. OK in Ubuntu)
-# x = ws - w
-# y = hs - h - 35  # -35 fixes it, more or less, for Win10
-
-# #set the dimensions of the screen and where it is placed
-# root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-#endregion other stuff old
 
 root.mainloop()
