8d44055b0006e09cd206b91c5bb7bac83cb21421
Tobgen69 <tobgen69@users.noreply.github.com>
2018-10-14 23:01:22 +0200

    Added Adjustable Times

--- Pomodoro.py
+++ Pomodoro.py
@@ -16,12 +16,15 @@ If possible I will add functionality gradually such as:
 Some tkinter docs: 
 https://www.tutorialspoint.com/python/python_gui_programming.htm
 http://effbot.org/tkinterbook/
+
+
+create executable with pyinstaller --noconsole Pomodoro.py in cmd
 """
 
 import tkinter as tk
 #from tkinter import messagebox 
 from datetime import datetime, date, timedelta
-import winsound # for the beep sound
+import winsound # for the beep soundg
 
 
 class Application(tk.Frame): # initialize a class to use later to create the application
@@ -37,17 +40,6 @@ class Application(tk.Frame): # initialize a class to use later to create the app
         tk.Frame.__init__(self, master) # ?? sets up the frame and shows/packs it
         self.pack()
 
-        tk.Label(self, text = "Pomodoro Time in minutes:", font=("Helvetica", 22)).pack()
-        self.timer_display = tk.StringVar() # initialize variable to display
-        self.timer_display.set('waiting...') # set default value if no update happend
-        tk.Label(self, textvariable = self.timer_display, font=("Helvetica", 128)).pack()
-
-        tk.Button(self, text='Exit', width = 10, bg = '#FF8080', command=root.destroy).pack() # create exit button
-        tk.Button(self, text='Start Countdown', command=self.CountdownButton).pack() # create CountdownButton
-        tk.Button(self, text='Pause', command = self.pause).pack() # create PauseButton
-
-        #messagebox.showinfo("Welcome", "Click on \"Start Countdown\" to start the Pomodoro cycle. ") # tutorial mode, don't show why in dev
-
         # Default attributes: // instead of global variables, no global because shall be possible to change
         self.pomo_run = 25*60  #/250 # /250 for testing purposes
         self.pomo_small = 5*60 #/250
@@ -60,6 +52,35 @@ class Application(tk.Frame): # initialize a class to use later to create the app
         self.t_pause_start = 0
         self.t_pause_end = 0
         self.t_pause_duration = timedelta(seconds = 0) # initialize t_pause_start for safe time when pause was started
+
+        tk.Label(self, text = "Pomodoro Time in minutes:", font=("Helvetica", 22)).pack()
+        self.timer_display = tk.StringVar() # initialize variable to display
+        self.timer_display.set('Start below') # set default value if no update happend
+        tk.Label(self, textvariable = self.timer_display, font=("Helvetica", 128)).pack()
+        self.pomo_c_display = tk.StringVar()
+        self.pomo_c_display.set('Pomodoros done: ' + str(0))        
+        tk.Label(self, textvariable = self.pomo_c_display, font=("Helvetica", 40)).pack()
+
+        tk.Button(self, text='Start Countdown', command=self.CountdownButton).pack() # create CountdownButton
+        tk.Button(self, text='Pause', command = self.pause).pack() # create PauseButton
+        tk.Button(self, text='Exit', width = 10, bg = '#FF8080', command=root.destroy).pack() # create exit button
+
+        # ENTRY FIELDS
+        tk.Label(self, text = "Set Pomodoro runtime:(sec) ").pack(side = "top")
+        self.pomo_run_custom = tk.StringVar(value = self.pomo_run)
+        self.entry_pomo_run = tk.Entry(self, textvariable = self.pomo_run_custom).pack(side = "top")
+
+        tk.Label(self, text = "Set small break runtime:(sec) ").pack(side = "top")
+        self.pomo_small_custom = tk.StringVar(value = self.pomo_small)
+        self.entry_pomo_small = tk.Entry(self, textvariable = self.pomo_small_custom).pack(side = "top")
+
+        tk.Label(self, text = "Set big break runtime:(sec) ").pack(side = "top")
+        self.pomo_big_custom = tk.StringVar(value = self.pomo_big)
+        self.entry_pomo_big = tk.Entry(self, textvariable = self.pomo_big_custom).pack(side = "top")
+
+        #messagebox.showinfo("Welcome", "Click on \"Start Countdown\" to start the Pomodoro cycle. ") # tutorial mode, don't show why in dev
+
+
 
       
     def CountdownRun(self):
@@ -79,8 +100,9 @@ class Application(tk.Frame): # initialize a class to use later to create the app
         else: 
             t_delta_display = str(timedelta(seconds = 0))[:-7] # set timer to 0, otherwise it is negative
             self.pomos_finished = 1 + int(self.pomo_counter/2) # can bet set to a label later to display in App # int floors doubles --> int(0.5)=0
-            self.pomo_counter += 1
+            self.pomo_counter += 1            
             self.timer_display.set(t_delta_display)
+            self.pomo_c_display.set('Pomodoros done: ' + str(self.pomos_finished))
             self.beep()
             #print(self.pomos_finished)  ## $%& TESTING     
 
@@ -91,7 +113,11 @@ class Application(tk.Frame): # initialize a class to use later to create the app
         """
 
         t_start = datetime.now() # set current datetime as start time
-        if self.pomo_counter%2 == 0:
+        self.pomo_run = int(self.pomo_run_custom.get())
+        self.pomo_small = int(self.pomo_small_custom.get())
+        self.pomo_big = int(self.pomo_big_custom.get())
+
+        if self.pomo_counter%2 == 0:            
             runtime_period = self.pomo_run # run normal pomodoro
         elif self.pomo_counter%7 ==0:
             runtime_period = self.pomo_big
